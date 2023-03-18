32b3dc056b291c851845a162b183be8366004fb9
sohampadhye007 <119999424+sohampadhye007@users.noreply.github.com>
2023-03-18 23:33:48 +0530

    Update HDR_new.py

--- HDR_new.py
+++ HDR_new.py
@@ -6,71 +6,55 @@ from skimage import io,  metrics
 import os
 from tqdm import tqdm
 
-EPSILON=sys.float_info.epsilon
+EPSILON = sys.float_info.epsilon
 
-# Define a function that applies the lightness transform to a given input value q
-def f(q, a=2.5, c=0.6, func=None):
-    # If a custom transform function is provided, use that instead
-    if func is not None:
-        return func(q)
-    # Otherwise, apply the default lightness transform
-    q_a = q ** a
-    return np.power((q_a / (1 + q_a)), c)
-
-# Define a function that applies the inverse lightness transform to a given input value f
-def f_inverse(f, a=2.5, c=0.6, func=None):
-    # If a custom inverse transform function is provided, use that instead
-    if func is not None:
-        return func(f)
-
-    # Otherwise, apply the default inverse lightness transform
-    f_cth_root = np.power(f, 1/c)
-    return np.power(f_cth_root / (1 - f_cth_root), 1/a)
-
-# Define a function to preprocess an image for deblurring
-def preprocess(image):
-    # Normalize pixels to [0, 1] range
-    image = image / 255.
-    # Clip values greater than or equal to 1 to avoid numerical issues
-    image[image >= 1] = 1 - EPSILON
-    return image
-
-# Define a function to deblur an image using the Wiener filter
-def deblur(image, n=5, m=15):
-    # Define the point spread function
-    psf = np.ones((n, n)) / m
-
-    # Separate the image into its RGB channels
-    B = image.reshape(image.shape[0], image.shape[1], 3)[:,:,0]
-    G = image.reshape(image.shape[0], image.shape[1], 3)[:,:,1]
-    R = image.reshape(image.shape[0], image.shape[1], 3)[:,:,2]
-
-    # Apply the Wiener filter to each channel
-    deconvolved_B = restoration.wiener(B, psf, 1, clip=False)
-    deconvolved_G = restoration.wiener(G, psf, 1, clip=False)
-    deconvolved_R = restoration.wiener(R, psf, 1, clip=False)
-
-    # Combine the filtered channels back into an RGB image
-    rgbArray = np.zeros((image.shape[0], image.shape[1], 3))
-    rgbArray[..., 0] = deconvolved_B
-    rgbArray[..., 1] = deconvolved_G
-    rgbArray[..., 2] = deconvolved_R
-
-    return rgbArray
-
-# Create a class for deblurring images using the lightness transform
-class LightSpaceDeblurrer:
-    def __init__(self, a=2.5, c=0.6, n=5, m=15):
-        self.a = a
-        self.c = c
-        self.n = n
-        self.m = m
-        self.psf = np.ones((self.n, self.n)) / self.m
+class ImageDeblur:
+    def __init__(self, image_path):
+        self.image_path = image_path
+        self.image = cv2.imread(image_path)
+        self.image = cv2.resize(self.image, (312, 312))
+        self.psf = np.ones((5, 5)) / 15
     
-    # Define a method to apply the lightness transform to an image
-    def apply_transform(self, image):
-        return f(image, self.a, self.c)
+    def preprocess(self):
+        self.image = self.image / 255.
+        self.image[self.image >= 1] = 1 - EPSILON
     
-    # Define a method to apply the inverse lightness transform to an image
-    def apply_inverse_transform(self, image):
-        return f_inverse
+    def deblur(self):
+        B = self.image.reshape(self.image.shape[0], self.image.shape[1], 3)[:,:,0]
+        G = self.image.reshape(self.image.shape[0], self.image.shape[1], 3)[:,:,1]
+        R = self.image.reshape(self.image.shape[0], self.image.shape[1], 3)[:,:,2]
+        deconvolved_B = restoration.wiener(B, self.psf, 1, clip=False)
+        deconvolved_G = restoration.wiener(G, self.psf, 1, clip=False)
+        deconvolved_R = restoration.wiener(R, self.psf, 1, clip=False)
+        rgbArray = np.zeros((self.image.shape[0], self.image.shape[1], 3))
+        rgbArray[..., 0] = deconvolved_B
+        rgbArray[..., 1] = deconvolved_G
+        rgbArray[..., 2] = deconvolved_R
+        self.normal_deblur = rgbArray
+    
+    def f(self, q, a=2.5, c=0.6, func=None):
+        if func is not None:
+            return func(q)
+        q_a = q**a
+        return np.power((q_a/(1+q_a)), c)
+    
+    def f_inverse(self, f, a=2.5, c=0.6, func=None):
+        if func is not None:
+            return func(f)
+        f_cth_root = np.power(f, 1/c)
+        return np.power(f_cth_root/(1-f_cth_root), 1/a)
+    
+    def lightspace_deblur(self):
+        self.lightspace_deblur = self.f_inverse(self.image)
+    
+    def show_result(self):
+        result = np.hstack((self.image, self.normal_deblur, self.lightspace_deblur))
+        cv2.imshow('result', result)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+image_deblur = ImageDeblur('night_blur4.jpg')
+image_deblur.preprocess()
+image_deblur.deblur()
+image_deblur.lightspace_deblur()
+image_deblur.show_result()
